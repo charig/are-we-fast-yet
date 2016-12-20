3cf39adc1a1c1815f4ddaeebee683edee908ff37
Guido Chari <charig@gmail.com>
2016-12-20 16:20:18 -0300

    Continue repository cleaning for Journal submission

--- implementations/setup.sh
+++ implementations/setup.sh
@@ -5,18 +5,11 @@ source $SCRIPT_PATH/script.inc
 
 ## Check for requirements
 check_for_tools git ant make mv uname cc c++
-check_for_crystal "non-fatal"
 check_for_node    "non-fatal"
-check_for ruby2.3 "" "non-fatal"
-check_for ruby    "Please see https://www.ruby-lang.org/en/documentation/installation/" "non-fatal"
 check_for_graalvm "non-fatal"
 
 $SCRIPT_PATH/build-java.sh
-$SCRIPT_PATH/build-crystal.sh
-$SCRIPT_PATH/build-jruby.sh
-$SCRIPT_PATH/build-som.sh
 $SCRIPT_PATH/build-mate.sh
-$SCRIPT_PATH/build-somns.sh
 $SCRIPT_PATH/build-graal.sh
 $SCRIPT_PATH/build-pharo.sh
 
