619767f6499febc7799eca01664ba290efc2508d
Guido Chari <charig@gmail.com>
2016-12-20 11:36:49 -0300

    Clean Repo for containing only required files for journal experiment

--- implementations/node.sh
+++ implementations/node.sh
@@ -1,11 +1,13 @@
 #/bin/sh
-if [ -x "`which nvm`" ]
-then
-  NODE_BIN=`nvm which 6`
-  if [ ! -x "$NODE_BIN" ] ; then
-    NODE_BIN=node
-  fi
-else
-  NODE_BIN=node
-fi
+#if [ -x "`which nvm`" ]
+#then
+#  $NODE_BIN=`nvm which 6`
+#  if [ ! -x "$NODE_BIN" ] ; then
+    NODE_BIN="/home/guido/.nvm/versions/node/v6.9.2/bin/node"
+#    NODE_BIN=node
+#  fi
+#else
+#  NODE_BIN=node
+#fi
+echo $NODE_BIN
 exec $NODE_BIN "$@"
